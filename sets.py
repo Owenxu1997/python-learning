0e401175fc150df3e3fd04242107932db3410383
Owenxu1997 <869975032@qq.com>
2018-01-25 14:46:38 +0800

    dictionary

--- sets.py
+++ sets.py
@@ -1,6 +1,24 @@
+#sets
+"""
 groceries={'牛奶','麦片'}
 print(groceries)
 if '牛奶' in groceries:
     print('你已经买了牛奶！')
 else:
     print('快去买牛奶')
+"""
+
+#dictionaries
+"""
+classmates={'owen': ' cute and funny', 'jenny': ' beautiful but short and has no boobies'}
+
+print(classmates['owen'])//这样可以打印item的value
+
+for k , v in classmates.items()://这样写就可以循环打印dictionary里面的item
+    print(k+v)
+"""
+
+#
+
+
+
